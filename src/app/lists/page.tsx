983f335b997244964eb20556f153c7a55955ae2d
i-mo76 <75702251+i-mo76@users.noreply.github.com>
2025-02-27 16:13:47 +0000

    end of section 5

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import ListsTab from './ListsTab'
+import { fetchCurrentUserLikeIds, fetchLikedMembers } from '../actions/likeActions'
 
-export default function ListsPage() {
+export default async function ListsPage({ searchParams }: { searchParams: Promise<{ type: string }> }) {
+  const { type } = await searchParams;
+
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await fetchLikedMembers(type);
+
   return (
-    <div>ListsPage</div>
+    <div>
+      <ListsTab members={members} likeIds={likeIds} />
+    </div>
   )
 }
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { getMembers } from '../actions/memberActions'
 import MemberCard from './MemberCard';
+import { fetchCurrentUserLikeIds } from '../actions/likeActions';
 
 export default async function MembersPage() {
   const members = await getMembers();
+  const likeIds = await fetchCurrentUserLikeIds();
 
   return (
     <>
@@ -12,7 +14,7 @@ export default async function MembersPage() {
         <>
           <div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
             {members && members.map(member => (
-              <MemberCard member={member} key={member.id} />
+              <MemberCard member={member} key={member.id} likeIds={likeIds}/>
             ))}
           </div>
         </>
